4e6c77c609b23f27180b99da9bce4cb62f0e931c
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-06-27 21:41:31 +0300

    added HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./redux/store";
@@ -9,6 +9,6 @@ import { router } from "./components/Router/routes";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <HashRouter router={router} />
   </Provider>
 );
