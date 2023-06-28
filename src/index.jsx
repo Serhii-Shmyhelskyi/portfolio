25d415a64d14711115d13f1b4c59b23825c89bff
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-06-28 11:51:37 +0300

    fixed refrash gh page error

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter router={router} />
+      <BrowserRouter basename="/repo-name" router={router} />
     </PersistGate>
   </Provider>
 );
