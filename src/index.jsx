e1d4aa423f871a139c3a25ecc647ffdb468931db
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-06-28 12:30:23 +0300

    add /

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/repo-name" router={router} />
+      <RouterProvider router={router} />
     </PersistGate>
   </Provider>
 );
