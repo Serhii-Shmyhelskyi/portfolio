43b5d21126eaaa9e12f5bda784adc069d31baeeb
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-06-28 11:41:25 +0300

    add br router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -11,7 +11,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <HashRouter router={router} />
+      <BrowserRouter router={router} />
     </PersistGate>
   </Provider>
 );
