b1194fa2c12d313dd3ee3b3b6445bf4d1f36297e
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-06-28 17:14:49 +0300

    fix link

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
   const [toggleMenu, setToggleMenu] = useState(true);
 
   let itemMenus = [
-    { name: "Home", link: "/" },
+    { name: "Home", link: "" },
     { name: "About me", link: "/me" },
     // { name: "Blog", link: "blog" },
     // { name: "Contacts", link: "contacts" },
@@ -17,7 +17,7 @@ const Menu = () => {
   let location = useLocation();
 
   let isActiveMenu = (button) =>
-    location.pathname === "/portfolio" + button.link;
+    location.pathname === "/portfolio/" + button.link;
   console.log(location.pathname);
 
   return (
