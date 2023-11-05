50624d73d0ee85d23edd1c29a66e283076af96cd
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-11-05 11:12:00 +0200

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,49 +12,60 @@ const Header = () => {
       <div className={styles.header_top}>
         <div className={styles.header_topLogo}>
           <Link to="/portfolio/">
-            <img width={100} src={Logo} alt="Logo" />
+            <img width={85} src={Logo} alt="Logo" />
           </Link>
         </div>
         <h1>Portfolio SHM</h1>
         <div className={styles.header_list}>
           <ul>
-            <li>
-              <img
-                src="https://img.icons8.com/ios-filled/256/phone.png"
-                alt="email"
-              />
-              <a href="tel:+380966265268">+380966265268</a>
-            </li>
-            <li>
-              <img
-                src="https://img.icons8.com/external-nawicon-glyph-nawicon/256/external-email-communication-nawicon-glyph-nawicon.png"
-                alt="phone"
-              />
-              <a href="mailto:shmserhi@gmail.com">shmserhi@gmail.com</a>
-            </li>
-            <a href="https://github.com/Serhii-Shmyhelskyi">
-              <li>
-                <img
-                  src="https://img.icons8.com/?size=512&id=12599&format=png"
-                  alt="gitHub"
-                />
-                <p className={styles.header_gh}>github</p>
-              </li>
-            </a>
-            <a href="https://www.linkedin.com/in/serhii-shmyhelskyi-99ba02200/">
-              <li>
-                <img
-                  src="https://img.icons8.com/?size=512&id=8808&format=png"
-                  alt="linkedin"
-                />
-                <p className={styles.header_gh}>Linkedin</p>
-              </li>
-            </a>
+            <div>
+              <a href="tel:+380966265268">
+                <li>
+                  <img
+                    src="https://img.icons8.com/ios-filled/256/phone.png"
+                    alt="email"
+                  />
+                  <p className={styles.header_gh}>+380966265268</p>
+                </li>
+              </a>
+              <a href="mailto:shmserhi@gmail.com">
+                <li>
+                  <img
+                    src="https://img.icons8.com/external-nawicon-glyph-nawicon/256/external-email-communication-nawicon-glyph-nawicon.png"
+                    alt="phone"
+                  />
+                  <p className={styles.header_gh}>shmserhi@gmail.com</p>
+                </li>
+              </a>
+            </div>
+            <div className={styles.header_listContact}>
+              <a href="https://github.com/Serhii-Shmyhelskyi" target="_blank">
+                <li>
+                  <img
+                    src="https://img.icons8.com/?size=512&id=12599&format=png"
+                    alt="gitHub"
+                  />
+                  <p className={styles.header_gh}>github</p>
+                </li>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/serhii-shmyhelskyi-99ba02200/"
+                target="_blank">
+                <li>
+                  <img
+                    src="https://img.icons8.com/?size=512&id=8808&format=png"
+                    alt="linkedin"
+                  />
+                  <p className={styles.header_gh}>Linkedin</p>
+                </li>
+              </a>
+            </div>
           </ul>
           <a
             className={styles.header_a}
-            href="https://www.linkedin.com/in/serhii-shmyhelskyi-99ba02200/">
-            <img width={100} src={Photo} alt="Serhii's photo" />
+            href="https://www.linkedin.com/in/serhii-shmyhelskyi-99ba02200/"
+            target="_blank">
+            <img width={70} src={Photo} alt="Serhii's photo" />
           </a>
         </div>
       </div>
